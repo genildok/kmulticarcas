345a9bae037d30b66e170dbe5477035ad204ffbd
genildok <genildok@hotmail.com>
2018-01-29 22:10:22 -0200

    Pagina detalhes produto - botões

--- resources/views/site/details/index.blade.php
+++ resources/views/site/details/index.blade.php
@@ -1,21 +1,19 @@
-@extends('site.layout.master')
+@extends('site.layout.master') @section('content')
 
-@section('content')
+<div class="container mt-5">
+    <div class="row mt-3">
+        <div class="col-12 col-md-6">
+            <div class="row row-content">
 
-    <div class="container">
-        <div class="card">
-            <div class="card-header bg-light p-0 m-0">
-                <h2 class="text-dark m-4">Camisa Polo - Reserva Original</h2>
-            </div>
-        <div class="row mt-3">
-            <div class="col-12 col-md-3">
-                <div class="row">
+                <div class="content-img">
 
-                    <div class="col-12">
-                        <img src="{{ asset('assets/site/img/product-img/01.jpg') }}" width="100%" alt="" class="img-responsive">
+                    <div class="col-12 col-md-12">
+                        <div class="">
+                            <img src="{{ asset('assets/site/img/product-img/01.jpg') }}" alt="" class="img-responsive">
+                        </div>
                     </div>
 
-                    <div class="col-12 mb-3 mt-2">
+                    <div class="col-12 col-md-12">
                         <ul class="list-inline">
                             <li class="list-inline-item d-inline">
                                 <img src="{{ asset('assets/site/img/product-img/01.jpg') }}" width="20%" alt="" class="img-thumbnail">
@@ -26,33 +24,82 @@
                             <li class="list-inline-item d-inline">
                                 <img src="{{ asset('assets/site/img/product-img/01.jpg') }}" width="20%" alt="" class="img-thumbnail">
                             </li>
-                         </ul>
-                    </div>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="card">
-            <div class="card-header pl-2 pt-2 pb-0 bg-light">
-                <h4>Produtos relacionados</h4>
+        <div class="col-md-6">
+            <div class="container">
+                <div class="form-group header">
+                    <h2 class="text-dark">Camisa Pólo - Reserva original</h2>
+                </div>
+                <div class="form-group mt-4 price">
+                    <span class="text-dark">Preço a Vista</span>
+                    <br>
+                    <h2 class="text-danger text-price">R$ 65,00</h2>
+                </div>
+
+                <div class="card p-2">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="lead">
+                                        <label for="color" class="text-secondary">Cores</label>
+                                    </div>
+                                    <div class="form-group">
+                                        <select name="color" id="color" class="form-control color">
+                                            <option value="1">Azul</option>
+                                            <option value="1">Amarelo</option>
+                                            <option value="1">Preto</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="lead">
+                                        <label for="quntity" class="text-secondary">Quantidade</label>
+                                    </div>
+                                    <div class="btn-group">
+                                        <input type="number" value="1" name="quantity" class="form-control quantity">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="lead">
+                                    <label for="quntity" class="text-secondary">Tamanhos</label>
+                                </div>
+                                <div class="btn-group" data-toggle="buttons">
+                                    <label for="" class="btn btn-primary" active>P
+                                        <input type="checkbox" name="size" id="size1" autocomplete="off" checked>
+                                    </label>
+                                    <label for="" class="btn btn-primary ml-3" active>M
+                                        <input type="checkbox" name="size" id="size1" autocomplete="off">
+                                    </label>
+                                    <label for="" class="btn btn-primary ml-3" active>G
+                                        <input type="checkbox" name="size" id="size1" autocomplete="off">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /row -->
+                        <hr class="my-3">
+                        <div class="form-group">
+                            <form action="{{ URL('/cart/add') }}" method="post">
+                                <div class="btn-group">
+                                    <button class="btn btn-warning" type="submit">
+                                        <i class="fa fa-shopping-cart"></i> Colocar no carrinho</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
-
-
-    <!-- <div class="row" style="border:1px solid #000;width:100px;">
-       <div class="col-md-5 col-12">
-         <div class="row" style="border:1px solid #000;width:100px;">
-             <div class="col-md-10 col-12" style="background: #fff;height:400px;border:1px solid #000">
-                <img src="{{ asset('assets/site/img/product-img/01.jpg') }}" width="200" alt="" class="img-responsive">
-             </div>
-             <div class="col-md-2 col-12 m-0 p-0" style="background: #ff3;height:400px;">
-                <img src="{{ asset('assets/site/img/product-img/01.jpg') }}"  alt="" class="img-thumbnail">
-             </div>
-         </div>
-
-       </div>
-    </div> -->
+</div>
 @endsection
--- resources/views/site/layout/includes/head.blade.php
+++ resources/views/site/layout/includes/head.blade.php
@@ -7,3 +7,5 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
   <!-- <link rel="stylesheet" href="./site/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"> -->
   <link rel="stylesheet" type="text/css" href="{{ asset('assets/site/css/home.css') }}">
+  <!-- Custom style details page -->
+  <link rel="stylesheet" type="text/css" href="{{ asset('assets/site/css/details.css') }}">
